e01ce5aaf85d68313e86c2fb8ffcdf194dfa547e
Julian Mesa <judmesabu@gmail.com>
2023-05-30 09:17:22 -0500

    Agregamos Navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+
+
+    <div className='container'>
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
